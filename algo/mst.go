82617c378a11c79000a0a467985b409480ea10e5
Bastian Katz <bastian.katz@hm.edu>
2025-04-14 18:52:54 +0200

    refactoring

--- algo/mst.go
+++ algo/mst.go
@@ -5,4 +5,7 @@ import (
 	"gitlab.lrz.de/hm/goal-core/hmgraph"
 )
 
-type MstAlgo[T base.Number] func(g *hmgraph.Graph, weight *hmgraph.EdgeMap[T]) (mst []*hmgraph.Edge)
+// A MsfAlgo is an algorithm finding a spanning forest for a given, undirected graph
+// that minimizes the total cost.
+// Panics if the graph is empty (no vertices) or contains any arcs.
+type MsfAlgo[T base.Number] func(g *hmgraph.Graph, cost *hmgraph.EdgeMap[T]) (mst []*hmgraph.Edge)
